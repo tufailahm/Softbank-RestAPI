33ca282b027af9bf5ed0c9339f86d8540e6d966e
tufailahm <tufailahmedkhan@gmail.com>
2025-09-09 11:58:51 +0530

    tufail commit code

--- src/main/java/com/training/softbankrestapi/controller/GreetingController.java
+++ src/main/java/com/training/softbankrestapi/controller/GreetingController.java
@@ -1,9 +1,11 @@
 package com.training.softbankrestapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("sayHi")
+@RestController
+@RequestMapping("sayHello")
 public class GreetingController {
 
     @GetMapping
